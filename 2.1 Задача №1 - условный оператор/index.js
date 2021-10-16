b83dace942ca37be0af49d49021ad270e4e7cbeb
jeniacernopiski47 <75862540+jeniacernopiski47@users.noreply.github.com>
2021-10-16 03:58:53 +0300

    Add files via upload

---
+++ 2.1 Задача №1 - условный оператор/index.js
@@ -0,0 +1,32 @@
+let n = prompt("Введите число?");
+switch (n) {
+  case '1':
+    alert('Один');
+    break;
+  case '2':
+    alert('Два');
+    break;
+  case '3':
+    alert('Три');
+    break;
+  case '4':
+    alert('Четыре');
+    break;
+  case '5':
+    alert('Пять');
+    break;
+  case '6':
+    alert('Шесть');
+    break;
+  case '7':
+    alert('Семь');
+    break;
+  case '8':
+    alert('Восемь');
+    break;
+  case '9':
+    alert('Девять');
+    break;
+  default:
+    alert('Неизвестное значение');
+}
