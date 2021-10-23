8f8f22fd921c0034ebc4c3a1f5d27ebc3349fb64
jeniacernopiski47 <75862540+jeniacernopiski47@users.noreply.github.com>
2021-10-23 14:35:37 +0300

    Add files via upload

---
+++ 2.8  Задача №8 - условный оператор/index.js
@@ -0,0 +1,15 @@
+
+var curentUserName = 'Jenia';
+var curentUserPassword = '0647';
+var InputName = prompt('Type your name ', curentUserName);
+var InputPassword = prompt('Type your password', curentUserPassword);
+
+if (curentUserName == InputName, curentUserPassword == InputPassword) {
+    alert("Hello Jenia");
+} else if (InputName, InputPassword == '') {
+    alert("Error Validation");
+} else {
+    alert("Error Password!");
+}
+
+
