b223f0efb9cdb6b8c5c1ffb22302ac1ee3f31d2c
jeniacernopiski47 <75862540+jeniacernopiski47@users.noreply.github.com>
2021-10-23 14:57:13 +0300

    Add files via upload

---
+++ 2.9  Задача №9 - взаимодействие с пользователем/index.js
@@ -0,0 +1,11 @@
+
+var curentUserName = prompt("Type your name", '');
+alert("Hello" + `${curentUserName}`);
+var QuestionforUser = confirm("Do you know java script?", '');
+if (QuestionforUser == true) {
+    alert("Well done")
+} else {
+    alert("You can start studying right now!");
+}
+
+
