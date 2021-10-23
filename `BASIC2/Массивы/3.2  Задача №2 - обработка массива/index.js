c6f35b40fcc841b7c2409f81ae4b6a26e97c61b9
jeniacernopiski47 <75862540+jeniacernopiski47@users.noreply.github.com>
2021-10-23 20:20:03 +0300

    Add files via upload

---
+++ `BASIC2/Массивы/3.2  Задача №2 - обработка массива/index.js
@@ -0,0 +1,22 @@
+let studentList = [
+  { name: "Ivan", marks: [8, 7, 6, 5, 8] },
+  { name: "Adrian", marks: [9, 8, 8, 7, 8] },
+  { name: "Andrei", marks: [6, 8, 9, 7, 7] },
+  { name: "Alex", marks: [10, 9, 9, 8, 10] },
+  { name: "Maxim", marks: [10, 10, 10, 10, 9] }
+];
+var max = studentList.reduce(( prev, cur ) => {
+  if (prev.b > cur.b) {
+    return prev
+  }
+  return cur;
+})
+let averages = studentList.map(s => {
+  return {
+    name: s.name,
+    av: s.marks.reduce((a, i) => a + i) / s.marks.length
+  };
+});
+console.log(studentList, averages);
+console.log(max);
+
