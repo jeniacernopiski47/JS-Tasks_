309e25cd8a1e48d1a48529fcb3ede9ffbcaa2da4
jeniacernopiski47 <75862540+jeniacernopiski47@users.noreply.github.com>
2021-10-25 20:16:02 +0300

    Add files via upload

---
+++ `BASIC2/Массивы/3.8 Задача №8 - функции обработки массивов/index.js
@@ -0,0 +1,10 @@
+
+const letters = ['red', 'green', 'blue']
+const index = letters.indexOf('green');
+function checkColor(arr) {
+    for (i = 0; i < arr.length; ++i) {
+        console.log(arr[i]);
+    }
+}
+console.log(letters);
+checkColor(console.log(index));
